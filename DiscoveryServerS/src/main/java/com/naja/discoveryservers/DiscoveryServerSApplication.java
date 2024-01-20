fe441c8e0005ef03c97f1692a7cd78157ce92d2d
Med Nj <razerhazer.2020@gmail.com>
2024-01-20 19:54:42 +0100

    first commit

---
+++ DiscoveryServerS/src/main/java/com/naja/discoveryservers/DiscoveryServerSApplication.java
@@ -0,0 +1,15 @@
+package com.naja.discoveryservers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerSApplication.class, args);
+	}
+
+}
---
+++ DiscoveryServerS/src/test/java/com/naja/discoveryservers/DiscoveryServerSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naja.discoveryservers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
